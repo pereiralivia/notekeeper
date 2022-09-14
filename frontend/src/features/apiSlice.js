2dbba6bb7dfed13562a601578a27e5e03320e5ca
Lívia Pereira <pereira.livia@gmail.com>
2022-09-14 16:10:56 -0300

    changed baseURL to production

--- frontend/src/features/apiSlice.js
+++ frontend/src/features/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://notesapplivia.herokuapp.com/",
-    baseUrl: "http://localhost:3001/"
+    baseUrl: "https://notesapplivia.herokuapp.com/",
+    // baseUrl: "http://localhost:3001/"
   }),
   tagTypes: ["Notes", "Note"],
   endpoints: (builder) => ({
